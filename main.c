5e1ec897a2a6cd3089a194e52ba5d9734c57b7b9
WebVladPasichnyk <114983141+WebVladPasichnyk@users.noreply.github.com>
2022-11-22 12:52:56 +0200

    Add files via upload
    Lab_8

--- main.c
+++ main.c
@@ -1,14 +1,37 @@
-#include "stdio.h"
-#include "math.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
-int main() {
-   float sum, average;
-   sum = 0;
-   int A[] = {5, 5, 4, 4, 4, 4, 4, 4, 4, 4, 5, 7, 8, 9, 10, 10, 9, 8, 7, 7, 6, 5, 5, 5, 5};
-   for (int i = 0; i < 24; i++)
-   {
-      sum = sum + A[i];
-   }
-   average = sum / 24;
-   printf("%f\n", average);
+float T = 0.03;
+int N = 200;
+
+int i;
+
+double sygn(float t)
+{
+    double s;
+
+    if (t == 0) s = 0;
+    if ((t > 0) && (t <= 0.4 * T)) s = 6 * t / (0.4 * T);
+    if ((t > 0.4 * T) && (t <= 0.08 )) s = 6;
+    if ((t > 0.8 * T) && (t <= T)) s = 6 - 6 * (t - 0.06) / 0.02;
+
+    return s;
+
 }
+int main ()
+ {
+    FILE* file = fopen("lab_8", "w+");
+    float h = T / N;
+    float t[N], s[N];
+    for (i = 0; i < N; i++ )
+    {
+        t[i] = i * h;
+        s[i] = sygn(t[i]);
+        fprintf(file, "%f    %f\n", t[i], s[i]);
+    }
+
+    fclose(file);   
+
+}
+
